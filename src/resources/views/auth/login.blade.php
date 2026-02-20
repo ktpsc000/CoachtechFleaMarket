5bae77cfc1d1ec81494dd5872ae20fa7895fd55b
root <root@S09-0813-pc.ohizumi-mfg.corp>
2026-02-20 10:21:27 +0900

    ER図修正　view作成中

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts/app')
 
 @section('css')
-<link rel="stylesheet" href="{{asset('css/items/index.css')}}">
+<link rel="stylesheet" href="{{asset('css/auth/login.css')}}">
 @endsection
 
 @section('content')
-<p class="test">商品一覧画面</p>
+
+<div class="login-form">
+    <h2 class="login-form__heading">ログイン</h2>
+    <div class="login-form__inner">
+        <form class="login-form__form" action="/login" method="post">
+            @csrf
+            <div class="login-form__group">
+                <label for="email" class="login-form__label">メールアドレス</label>
+                <input class="login-form__input" type="email" name="email" id="email">
+            </div>
+            <div class="login-form__group">
+                <label for="password" class="login-form__label">パスワード</label>
+                <input class="login-form__input" type="password" name="password" id="password">
+            </div>
+            <input class="login-form__btn" type="submit" value="ログインする">
+        </form>
+        <a href="/register" class="login-page__register-link">会員登録はこちら</a>
+    </div>
+</div>
+
 @endsection
--- src/resources/views/mypage/edit.blade.php
+++ src/resources/views/mypage/edit.blade.php
@@ -5,5 +5,42 @@
 @endsection
 
 @section('content')
-<p class="test">商品一覧画面</p>
+
+@extends('layouts/app')
+
+@section('css')
+<link rel="stylesheet" href="{{asset('css/auth/profile.css')}}">
+@endsection
+
+@section('content')
+
+
+<div class="profile-form">
+    <h2 class="profile-form__heading">プロフィール編集</h2>
+    <div class="profile-form__inner">
+        <form class="profile-form__form" action="/mypage" method="post">
+            @csrf
+            <div class="profile-form__group">
+                <label for="name" class="profile-form__label">ユーザー名</label>
+                <input class="profile-form__input" type="text" name="name" id="name">
+            </div>
+            <div class="profile-form__group">
+                <label for="postal_code" class="profile-form__label">郵便番号</label>
+                <input class="profile-form__input" type="number" name="postal_code" id="postal_code">
+            </div>
+            <div class="profile-form__group">
+                <label for="address" class="profile-form__label">住所</label>
+                <input class="profile-form__input" type="text" name="address" id="address">
+            </div>
+            <div class="profile-form__group">
+                <label for="building" class="profile-form__label">建物名</label>
+                <input class="profile-form__input" type="text" name="building" id="building">
+            </div>
+            <input class="profile-form__btn" type="submit" value="更新する">
+        </form>
+    </div>
+</div>
+
+@endsection
+
 @endsection
