2b5c3c1a80bd2fa564ba35e18832442cf5948b6e
root <root@S09-0813-pc.ohizumi-mfg.corp>
2026-03-17 17:08:06 +0900

    商品詳細CSS修正中

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -39,46 +39,54 @@
 
         <div class="show-info__description">
             <h2 class="show-info__title">商品説明</h2>
-            <p class="show-info__content">{{$item->description}}</p>
+            <p class="show-info__description-text"></p>{{$item->description}}
         </div>
 
         <div class="show-info__detail">
+
             <h2 class="show-info__title">商品の情報</h2>
-            <div class="show-info__content">
-                <p class="show-info__content-title">カテゴリー</p>
+
+            <div class="show-info__category">
+                <p class="show-info__category-title">カテゴリー</p>
                 @foreach($item->categories as $category)
-                <span class="show-info__category">{{$category->name}}</span>
+                <span class="show-info__category-name">{{$category->name}}</span>
                 @endforeach
             </div>
-            <div class="show-info__content">
-                <p class="show-info__content-title">商品の状態</p>
-                <p class="show-info__condition">{{$item->condition_text}}</p>
+
+            <div class="show-info__condition">
+                <p class="show-info__condition-title">商品の状態</p>
+                <p class="show-info__condition-text">{{$item->condition_text}}</p>
             </div>
 
-            <div class="show-info__comment">
-                <h2 class="show-info__comment-title">コメント({{ $item->comments_count }})</h2>
-                <div class="show-info__comment-list">
-                    @foreach($item->comments as $comment )
-                    <div class="show-info__comment-user">
-                        <img src="" alt="プロフィール画像">
-                        <p>{{$comment->user->profile->name}}</p>
-                    </div>
-                    <p>{{$comment->content}}</p>
-                    @endforeach
-                </div>
-                <div class="show-info__comment-create">
-                    <h3>商品へのコメント</h3>
-                    <form action="/item/{{$item->id}}" method="post">
-                        @csrf
-                        <input class="show-info__comment-create--input" type="text" name="content">
-                        @error('content')
-                        {{ $message }}
-                        @enderror
-                        <button class="show-info__comment-create--submit" type="submit">コメントを送信する</button>
-                    </form>
-                </div>
-            </div>
         </div>
+
+        <div class="show-info__comment">
+            <h2 class="show-info__comment-title">コメント({{ $item->comments_count }})</h2>
+
+            @foreach($item->comments as $comment )
+            <div class="show-info__comment-list">
+                <div class="show-info__comment-user">
+                    <img src="" alt="">
+                    <p>{{$comment->user->profile->name}}</p>
+                </div>
+                <p class="show-info__comment-text">{{$comment->content}}</p>
+            </div>
+            @endforeach
+
+            <div class="show-info__comment-store">
+                <h3>商品へのコメント</h3>
+                <form action="/item/{{$item->id}}" method="post">
+                    @csrf
+                    <input class="show-info__comment-store--input" type="text" name="content">
+                    @error('content')
+                    {{ $message }}
+                    @enderror
+                    <button class="show-info__comment-store--submit" type="submit">コメントを送信する</button>
+                </form>
+            </div>
+
+        </div>
+
     </div>
 </div>
 
